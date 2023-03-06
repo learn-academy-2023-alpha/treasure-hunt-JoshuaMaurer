f8da9e6c19148caabafd350fe2fa4bf344ffd034
Josh Maurer <maurer.joshua@gmail.com>
2023-03-06 15:00:25 -0800

    Revert "added index being clicked to App.js"

--- src/App.js
+++ src/App.js
@@ -15,14 +15,10 @@ const App = () => {
         "?",
         "?"
     ])
-
-    const handleGamePlay = (index) => {
-        alert (index)
-    }
 // value is being passed through Square as props
 // key={index} is only used in react when iterating over something
 // if more than 1 parameter passed through {board.map()} an extra set of () needed around the parameters
-// best practice to keep key={index} seperate from index={index}. Especually when destructuring.
+// 
     return (
         <>
             <h1>Treasure Hunt Game</h1>
@@ -33,8 +29,6 @@ const App = () => {
                         <Square 
                             value={value} 
                             key={index}
-                            index={index}
-                            handleGamePlay={handleGamePlay}
                         />
                     )
                 })}
