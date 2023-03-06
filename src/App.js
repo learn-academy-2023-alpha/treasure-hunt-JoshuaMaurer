36fad275f8dfa1b28b7714177da14c196f0e03b0
JoshuaMaurer <maurer.joshua@gmail.com>
2023-03-06 14:14:18 -0800

    sets up basic grid for treasure hunt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, { useState } from "react"
+import Square from "./components/Square"
+
 import "./App.css"
 
 const App = () => {
-  const [board, setBoard] = useState([
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?",
-    "?"
-  ])
-
-  return (
-    <>
-      <h1>Treasure Hunt Game</h1>
-    </>
-  )
+    const [board, setBoard] = useState([
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?"
+    ])
+// value is being passed through Square as props
+// key={index} is only used in react when iterating over something
+// if more than 1 parameter passed through {board.map()} an extra set of () needed around the parameters
+// 
+    return (
+        <>
+            <h1>Treasure Hunt Game</h1>
+            
+            <div className="gameboard">
+                {board.map((value, index) => {
+                    return (
+                        <Square 
+                            value={value} 
+                            key={index}
+                        />
+                    )
+                })}
+            </div>
+            
+        </>
+    )
 }
 
 export default App
